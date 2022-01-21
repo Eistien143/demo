6d498b55bcdaffaab9eebaa52a3ec57460acc0dd
Arun <eistnewtramanujan00000@gmail.com>
2022-01-21 11:04:49 +0530

    project initialised

---
+++ src/components/Card.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <div class="card">
+      <h1>{{ title }}</h1>
+      <p>
+        {{ description }}
+      </p>
+      <h3>{{ ammount }}</h3>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: { title: String, description: String, ammount: Number },
+};
+</script>
+
+<style>
+.card {
+  width: 300px;
+  height: 200px;
+  background-color: rgb(150, 150, 150);
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <h3>this is home page</h3>
+    <Card
+      v-for="(mobile, i) in carts"
+      :key="i"
+      :title="mobile.name"
+      :description="mobile.details"
+      :ammount="mobile.price"
+    ></Card>
+  </div>
+</template>
+
+<script>
+import Card from "@/components/Card.vue";
+
+export default {
+  components: { Card },
+
+  data() {
+    return {
+      carts: [
+        {
+          name: "Samsung Mobile",
+          details: "this is a samsung phone",
+          price: "2000 Rs",
+        },
+        {
+          name: "Jio Mobile",
+          details: "this is a Jio phone",
+          price: 90000,
+        },
+        {
+          name: "Samsung Mobile",
+          details: "this is a samsung phone",
+          price: 7000,
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style></style>
